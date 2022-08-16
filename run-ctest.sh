e0eb95ca9e7f8eadc9f1c5fc6e73d3882bbadcc5
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2022-08-16 18:52:39 -0500

    Make sure the script starts in the right directory

--- run-ctest.sh
+++ run-ctest.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 BUILD_TYPE=Release docker-compose -f docker-compose.yml -f docker-compose-ctest.yml build
 DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 BUILD_TYPE=Release docker-compose -f docker-compose.yml -f docker-compose-ctest.yml run openscad
